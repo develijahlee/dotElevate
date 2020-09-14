0701c8a1843904fa3e4d99f77a2982ce4e82bc3b
Elijah Lee <develijahlee@gmail.com>
2020-09-14 22:03:46 +0900

    simplified router template

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <AboutWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import AboutWorld from "@/components/AboutWorld.vue";
+
+export default {
+  name: "about",
+  components: {
+    AboutWorld,
+  },
+};
+</script>
